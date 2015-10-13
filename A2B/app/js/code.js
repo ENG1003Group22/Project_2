b3b2e0cc801bf5768f5d67986c0e4c1354a811c6
Xavier Thompson <xjtho1@student.monash.edu>
2015-10-13 19:07:04 +1100

    First Working Version
    Test if your keen.

--- A2B/app/js/code.js
+++ A2B/app/js/code.js
@@ -1,10 +1,11 @@
 //variables Common to all functions
 var isTracking = false; //The Website will start tracking the user
 var route = [];
-var timeInterval = 1; //seconds
+//var timeInterval = 1; //seconds
 var allowedAccuracy = 0;
 var simplifiedDistance = 25;
 var map; //The map from Google Maps API
+var trackID;
 
 //var button1 = document.getElementById("SSButton").innerHTML;
 
@@ -12,14 +13,14 @@ var map; //The map from Google Maps API
 
 //document.getElementById("outputArea").innerHTML = "jkgiklgiuyyg";
 
-function addLocation(){
+function trackLocation(){
     if (navigator.geolocation)
     {   var positionOptions = {
             enableHighAccuracy: true,
             timeout: 10000, 
             maximumAge: 0}; 
 
-    navigator.geolocation.getCurrentPosition(showCurrentLocation, errorHandler, positionOptions);
+    trackID = navigator.geolocation.watchPosition(showCurrentLocation, errorHandler, positionOptions);
     }
     
     function errorHandler(error)
@@ -40,12 +41,13 @@ function addLocation(){
     currentLoc = {
     lat:Number(position.coords.latitude),
     lng:Number(position.coords.longitude),
-    acc:Number(position.coords.accuracy),
+    //acc:Number(position.coords.accuracy),
     time:time()};
      
-    document.getElementById("outputArea").innerHTML="Lat: "+currentLoc.lat+"</br>Lng: "+currentLoc.lng; 
+    document.getElementById("outputArea").innerHTML="Lat: "+currentLoc.lat+"</br>Lng: "+currentLoc.lng+"</br>Route: "+route.length; 
      
-    route.push(currentLoc)
+    route.push(currentLoc);
+    displayPath(route);
     console.log(currentLoc);
     
      function time(){
@@ -54,74 +56,43 @@ function addLocation(){
          return n};
      
     }; 
-}// END addLocation
+}// END trackLocation
 
-
-function initMap() {
-    var currentPosition;
-    
-            //Coords of Current Position
-            
-    
-            //Actual Map -- https://developers.google.com/maps/documentation/javascript/
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: {
-                    lat: -37.8749147,
-                    lng: 145.0468735
-                },
-                zoom: 17
-            });
-            
-            //var abc = document.getElementById('abc');
-            //abc.className = "abc"
-        } //END INITMAP
-
-
- function trackingToggle(){
+function trackingToggle(){
      var tracking;
      
         if(isTracking === true){
-            clearInterval(tracking); // Stops the time interval from repeating the Tracking Function
+            //clearInterval(tracking); // Stops the time interval from repeating the Tracking Function
+            navigator.geolocation.clearWatch(trackID);
             isTracking = false;
             document.getElementById("SSButton").innerHTML = "START";
             document.getElementById("SSButton").style.background = "rgb(76, 175, 80)"
             //saveToMemory(route); //Saves to memory for use in the "history" tab
+            endMarker(route) // Plots the endpoint on the map
             route = []; // Clears the route so that anothing route can be recorded
             console.log("Tracking-Off")
 		}
         else{
             isTracking = true;
-            trackingLocation(route, timeInterval); //Starting tracking the position at the timeInterval
+            //trackingLocation(route, timeInterval); //Starting tracking the position at the timeInterval
+            trackLocation()
             document.getElementById("SSButton").innerHTML = "STOP";
             document.getElementById("SSButton").style.background = "rgb(244, 67, 54)"
             console.log("Tracking-On")
         }
     
-function trackingLocation(route, timeInterval){
+/*function trackingLocation(route, timeInterval){
     var timeIntervalMS;
     
             //convert timeInterval to milaseconds
             timeIntervalMS = timeInterval*1000;
     
             //Add a coord to Route and display every <time interval>
-            tracking = setInterval(addLocAndUpdate(route),timeInterval);
+            tracking = setInterval(addLocation(),timeIntervalMS);
             
-        }//END trackingLocation
+        }//END trackingLocation */ //For use with navigator.geolocation.getCurrentPosition()
      
-     } //FUNCTION trackingToggle
-            
-function addLocAndUpdate(route){   
-    
-        //Adds the new Location to Route and Displays current location on screen
-    addLocation()   
-
-    
-    //display updated route
-    displayPath(route)
-    
-}// END addLocAndUpdate        
-    
-
+     } //END trackingToggle
     
     function displayPath(route){
     var path, coords, startPoint;
@@ -149,21 +120,30 @@ function addLocAndUpdate(route){
 
 }//END displayPath
     
-
-
-
+    function endMarker(route){
+ var endPoint;
+    
+    endPoint = new google.maps.Marker({
+        position: route[route.length - 1],
+        map: map,
+        title: "End Point"
+    })
+}//END endMarker
 
             
-function totalDistance(){
-    var distance;
+function totalDistance(route){
+    var distance = 0;
     
+    for (i=0;i<=(route.length - 1);i++){
+        distance = distance + distance2Points(i, i+1);                        
+    }                    
     
-     return distance;
+    return distance; //in meters
     
     function distance2Points(start, end){
-    var R = 6371000, a, b, c, distance, lat1, lat2, lng1, lng2; // metres
+    var R = 6371000, a, c, distance, lat1, lat2, lng1, lng2; // metres
     
-    lat1 = start.lat; lng1 = start.lat;
+    lat1 = start.lat; lng1 = start.lng;
     lat2 = end.lat; lng2 = end.lng;
     
 var x1 = lat1.toRadians();
@@ -178,17 +158,40 @@ var dy = (lng2-lng1).toRadians();
     }
 }
 
-function averageSpeed(){
+function duration(route){
+    var time = route[route.length - 1].time - route[0].time; // In Millerseconds
+        
+        time = time*0.001;
+        
+    return time;      //in seconds                  
+}
+                        
+function averageSpeed(time, distance){
+        var speed = distance/time; // should be in meters/second
     
 }
 
-function duration(){
+
+function saveToMemory(route){
     
 }
 
-function saveToMemory(){
+function initMap() {
+    var currentPosition;
     
-}
-
-
+            //Coords of Current Position
+            
+    
+            //Actual Map -- https://developers.google.com/maps/documentation/javascript/
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: {
+                    lat: -37.8749147,
+                    lng: 145.0468735
+                },
+                zoom: 17
+            });
+            
+            //var abc = document.getElementById('abc');
+            //abc.className = "abc"
+        } //END INITMAP
     
